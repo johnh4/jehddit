c4da8e76bd364ec9284fa038c860a5e345292798
johnh4 <jehowl4@gmail.com>
2013-09-12 19:32:26 -0400

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Jehddit::Application.config.secret_key_base = '1c7b1ff524f64546e3fbc19d4d3f6787c499b52dd02ac47f0a90ff3f190c4f0fab87aaf6b8caf9cb8a9b3c67b92dcf0e632a2eb1798009aac07d5c63699bd476'
