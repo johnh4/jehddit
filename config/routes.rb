9388a68a136c1906b9d2fd4d14f48d68692c2011
johnh4 <jehowl4@gmail.com>
2013-09-13 00:45:11 -0400

    Add users, signin/out.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Jehddit::Application.routes.draw do
-  get "static_pages/about"
-  get "static_pages/contact"
-  get "static_pages/help"
-
+  
   get 'help', to: "static_pages#help"
   get 'about', to: "static_pages#about"
   get 'contact', to: "static_pages#contact"
+
+  get 'signin', to: "sessions#new"
+  delete 'signout', to: "sessions#destroy"
+  resources :users
+  resources :sessions, only: [:new, :create, :destroy]
 
   root "static_pages#home"
 
